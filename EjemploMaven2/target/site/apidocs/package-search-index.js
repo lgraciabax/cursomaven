be0449963cb399d8f154a343d12c50e6cd6b013e
Sergio3157 <SergioMoralesS@axity.com>
2023-06-09 18:01:00 -0600

    Se modifico jenkins

--- EjemploMaven2/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.axity.curso.maven2"}]
---
+++ services/webapp/src/test/java/com/axity/curso/maven2/webapp/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axity.curso.maven2.webapp.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
